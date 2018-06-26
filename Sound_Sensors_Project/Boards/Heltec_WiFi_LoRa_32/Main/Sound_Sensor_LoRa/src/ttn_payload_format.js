95e9d2512f96ff740df602c7d9931e69c1e88587
Javier V <javier.vargas@podgroup.com>
2018-06-26 12:19:50 +0200

    Updated decoder to parse floats instead of strings

--- Sound_Sensors_Project/Boards/Heltec_WiFi_LoRa_32/Main/Sound_Sensor_LoRa/src/ttn_payload_format.js
+++ Sound_Sensors_Project/Boards/Heltec_WiFi_LoRa_32/Main/Sound_Sensor_LoRa/src/ttn_payload_format.js
@@ -1,4 +1,7 @@
 // Function to decode payload sent to The Things Network
+// This goes under https://console.thethingsnetwork.org/
+// Applications > <your_app> > Payload Formats
+
 // This take the payload and translate to a JSON with
 // 3 parameters: max sound, min sound, average sound
 
@@ -8,13 +11,13 @@ function Decoder(bytes, port) {
   var dBA_string = String.fromCharCode.apply(null, bytes);
   dBA_string = dBA_string.split('\00').join('');
   dBA_string = dBA_string.split(';');
-
-
+  
+  
   // Parsing to a float
   return {
     //dBA: parseFloat(dBA_string
-    maximum_dBA: dBA_string[0],
-    minimum_dBA: dBA_string[1],
-    average_dBA: dBA_string[2]
+    maximum_dBA: parseFloat(dBA_string[0]),
+    minimum_dBA: parseFloat(dBA_string[1]),
+    average_dBA: parseFloat(dBA_string[2])
   };
 }
